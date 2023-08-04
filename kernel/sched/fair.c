82a3ef41e15ef59b3f6249a7a5a99558e9062dd4
EmanuelCN <emanuelghub@gmail.com>
2023-08-04 16:01:07 +0000

    kernel: sched: Revert upstream scheduler changes
        Revert "UPSTREAM: sched/topology: Allow sched_asym_cpucapacity to be disabled"
    
        Revert "sched/fair: Fix unfairness caused by missing load decay"
    
        Revert "sched/fair: Make sure to update tg contrib for blocked load"
    
        Revert "BACKPORT: sched/fair: Fix overutilized update in enqueue_task_fair()"
    
        Revert "sched/fair: Optimize select_idle_cpu"
    
        Revert "sched/fair: Fix CFS bandwidth hrtimer expiry type"
    
        Revert "sched/debug: Fix cgroup_path[] serialization"
    
        Revert "sched/deadline: Fix sched_dl_global_validate()"
    
        Revert "sched: Reenable interrupts in do_sched_yield()"
    
        Revert "UPSTREAM: sched: Fix out-of-bound access in uclamp"
    
    Reason: Performance regression
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6034,7 +6034,7 @@ static const u64 cfs_bandwidth_slack_period = 5 * NSEC_PER_MSEC;
 static int runtime_refresh_within(struct cfs_bandwidth *cfs_b, u64 min_expire)
 {
 	struct hrtimer *refresh_timer = &cfs_b->period_timer;
-	s64 remaining;
+	u64 remaining;
 
 	/* if the call-back is running a quota refresh is already occurring */
 	if (hrtimer_callback_running(refresh_timer))
@@ -6042,7 +6042,7 @@ static int runtime_refresh_within(struct cfs_bandwidth *cfs_b, u64 min_expire)
 
 	/* is a quota refresh about to occur? */
 	remaining = ktime_to_ns(hrtimer_expires_remaining(refresh_timer));
-	if (remaining < (s64)min_expire)
+	if (remaining < min_expire)
 		return 1;
 
 	return 0;
@@ -10175,7 +10175,7 @@ static bool __update_blocked_fair(struct rq *rq, bool *done)
 		/* Propagate pending load changes to the parent, if any: */
 		se = cfs_rq->tg->se[cpu];
 		if (se && !skip_blocked_update(se))
-			update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
+			update_load_avg(cfs_rq_of(se), se, 0);
 
 		/*
 		 * There can be a lot of idle CPU cgroups.  Don't let fully
