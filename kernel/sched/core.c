32e1a3eda621f1e630b8d5c4e6eb882da4f7c81e
deu <fawwazzuladhim700@gmail.com>
2026-05-17 01:21:58 +0700

    sched/uclamp: register uclamp cpuctl
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8591,6 +8591,26 @@ static struct cftype cpu_files[] = {
 		.write_u64 = cpu_rt_period_write_uint,
 	},
 #endif
+#ifdef CONFIG_UCLAMP_TASK_GROUP
+	{
+		.name = "uclamp.min",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_min_show,
+		.write = cpu_uclamp_min_write,
+	},
+	{
+		.name = "uclamp.max",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_max_show,
+		.write = cpu_uclamp_max_write,
+	},
+	{
+		.name = "uclamp.latency_sensitive",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_u64 = cpu_uclamp_ls_read_u64,
+		.write_u64 = cpu_uclamp_ls_write_u64,
+	},
+#endif
 	{ }	/* Terminate */
 };
 
