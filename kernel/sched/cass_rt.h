69e1b38fc7d3c0c9a1e49113503029a6fd90b715
deu <fawwazzuladhim700@gmail.com>
2026-06-02 08:13:52 +0700

    sched/rt: wire up cass-based rt scheduling
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

---
+++ kernel/sched/cass_rt.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2024 Sultan Alsawaf <sultan@kerneltoast.com>.
+ */
+
+#ifdef CONFIG_SCHED_CASS
+int cass_select_task_rq_rt(struct task_struct *p, int prev_cpu, int wake_flags);
+
+/* Use CASS. A dummy wrapper ensures the replaced function is still "used". */
+static inline void *select_task_rq_rt_dummy(void)
+{
+	return (void *)select_task_rq_rt;
+}
+/* compatibility shim for 4.14 */
+static int select_task_rq_rt_5to3(struct task_struct *p, int prev_cpu,
+				   int sd_flag, int wake_flags, int sibling_count_hint)
+{
+	return cass_select_task_rq_rt(p, prev_cpu, wake_flags);
+}
+#define select_task_rq_rt select_task_rq_rt_5to3
+#endif /* CONFIG_SCHED_CASS */
