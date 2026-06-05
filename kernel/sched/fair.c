c150a4d2b2be7fa3720613003f6016387e55d8a9
deu <fawwazzuladhim700@gmail.com>
2026-06-05 21:19:30 +0700

    sched/fair: refactor update_load_avg
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3994,9 +3994,8 @@ static void detach_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 #define DO_ATTACH	0x4
 
 /* Update task and its cfs_rq load average */
-static inline void update_load_avg(struct sched_entity *se, int flags)
+static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
-	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 	u64 now = cfs_rq_clock_pelt(cfs_rq);
 	struct rq *rq = rq_of(cfs_rq);
 	int cpu = cpu_of(rq);
@@ -4519,7 +4518,7 @@ enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 *     its group cfs_rq
 	 *   - Add its new weight to cfs_rq->load.weight
 	 */
-	update_load_avg(se, UPDATE_TG | DO_ATTACH);
+	update_load_avg(cfs_rq_of(se), se, UPDATE_TG | DO_ATTACH);
 	enqueue_entity_load_avg(cfs_rq, se);
 	update_cfs_group(se);
 	account_entity_enqueue(cfs_rq, se);
@@ -4604,7 +4603,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 				goto skip_delay;
 			if (tsk && (tsk->flags & PF_FREEZER_SKIP || freezing(tsk) || frozen(tsk)))
 				goto skip_delay;
-			update_load_avg(se, 0);
+			update_load_avg(cfs_rq_of(se), se, 0);
 			update_entity_lag(cfs_rq, se);
 			set_delayed(se);
 			return false;
@@ -4612,7 +4611,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 		skip_delay:
 	}
 
-	update_load_avg(se, UPDATE_TG);
+	update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 	dequeue_entity_load_avg(cfs_rq, se);
 
 	update_stats_dequeue(cfs_rq, se, flags);
@@ -4653,7 +4652,7 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, bool first)
 		clear_buddies(cfs_rq, se);
 		update_stats_wait_end(cfs_rq, se);
 		__dequeue_entity(cfs_rq, se);
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 
 		if (first)
 			set_protect_slice(cfs_rq, se);
@@ -4720,7 +4719,7 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 		/* Put 'current' back into the tree. */
 		__enqueue_entity(cfs_rq, prev);
 		/* in !on_rq case, update occurred at dequeue */
-		update_load_avg(prev, 0);
+		update_load_avg(cfs_rq_of(prev), prev, 0);
 	}
 	cfs_rq->curr = NULL;
 }
@@ -4736,7 +4735,7 @@ entity_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr, int queued)
 	/*
 	 * Ensure that runnable average is periodically updated.
 	 */
-	update_load_avg(curr, UPDATE_TG);
+	update_load_avg(cfs_rq_of(curr), curr, UPDATE_TG);
 	update_cfs_group(curr);
 
 #ifdef CONFIG_SCHED_HRTICK
@@ -5690,7 +5689,7 @@ requeue_delayed_entity(struct sched_entity *se)
 		cfs_rq->nr_queued++;
 	}
 
-	update_load_avg(se, 0);
+	update_load_avg(cfs_rq_of(se), se, 0);
 	clear_delayed(se);
 }
 
@@ -5785,7 +5784,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_throttled(cfs_rq))
 			break;
 
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 		update_cfs_group(se);
 	}
 
@@ -5867,7 +5866,7 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
 
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 		se_update_runnable(se);
 		update_cfs_group(se);
 
@@ -9396,7 +9395,7 @@ static void put_prev_task_fair(struct rq *rq, struct task_struct *prev)
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
-		update_load_avg(se, 0);
+		update_load_avg(cfs_rq_of(se), se, 0);
 		put_prev_entity(cfs_rq, se);
 	}
 }
@@ -10156,7 +10155,7 @@ static void __update_blocked_averages(struct rq *rq)
 		/* Propagate pending load changes to the parent, if any: */
 		se = cfs_rq->tg->se[cpu];
 		if (se && !skip_blocked_update(se))
-			update_load_avg(se, 0);
+			update_load_avg(cfs_rq_of(se), se, 0);
 	}
 	update_rt_rq_load_avg(rq_clock_pelt(rq), rq, 0);
 	update_dl_rq_load_avg(rq_clock_pelt(rq), rq, 0);
@@ -12892,7 +12891,7 @@ static void propagate_entity_cfs_rq(struct sched_entity *se)
 		if (cfs_rq_throttled(cfs_rq))
 			break;
 
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 	}
 }
 #else
@@ -12904,7 +12903,7 @@ static void detach_entity_cfs_rq(struct sched_entity *se)
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 
 	/* Catch up with the cfs_rq and remove our load when we leave */
-	update_load_avg(se, 0);
+	update_load_avg(cfs_rq_of(se), se, 0);
 	detach_entity_load_avg(cfs_rq, se);
 	update_tg_load_avg(cfs_rq, false);
 	propagate_entity_cfs_rq(se);
@@ -12923,7 +12922,7 @@ static void attach_entity_cfs_rq(struct sched_entity *se)
 #endif
 
 	/* Synchronize entity with its cfs_rq */
-	update_load_avg(se, sched_feat(ATTACH_AGE_LOAD) ? 0 : SKIP_AGE_LOAD);
+	update_load_avg(cfs_rq_of(se), se, sched_feat(ATTACH_AGE_LOAD) ? 0 : SKIP_AGE_LOAD);
 	attach_entity_load_avg(cfs_rq, se);
 	update_tg_load_avg(cfs_rq, false);
 	propagate_entity_cfs_rq(se);
@@ -13240,7 +13239,7 @@ int sched_group_set_shares(struct task_group *tg, unsigned long shares)
 		rq_lock_irqsave(rq, &rf);
 		update_rq_clock(rq);
 		for_each_sched_entity(se) {
-			update_load_avg(se, UPDATE_TG);
+			update_load_avg(cfs_rq_of(se), se, UPDATE_TG);
 			update_cfs_group(se);
 		}
 		rq_unlock_irqrestore(rq, &rf);
