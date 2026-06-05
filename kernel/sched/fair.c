d66e6a827d191d72817da35c00584ce12c544735
deu <fawwazzuladhim700@gmail.com>
2026-06-05 23:15:14 +0700

    sched/fair: fix locking mismatch in remove_entity_load_avg
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4076,24 +4076,21 @@ void sync_entity_load_avg(struct sched_entity *se)
 void remove_entity_load_avg(struct sched_entity *se)
 {
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
+	unsigned long flags;
 
 	/*
 	 * tasks cannot exit without having gone through wake_up_new_task() ->
 	 * post_init_entity_util_avg() which will have added things to the
 	 * cfs_rq, so we can remove unconditionally.
-	 *
-	 * Similarly for groups, they will have passed through
-	 * post_init_entity_util_avg() before unregister_sched_fair_group()
-	 * calls this.
 	 */
 
 	sync_entity_load_avg(se);
-	raw_spin_lock(&cfs_rq->removed.lock);
+	raw_spin_lock_irqsave(&cfs_rq->removed.lock, flags);
 	cfs_rq->removed.nr++;
 	cfs_rq->removed.load_avg += se->avg.load_avg;
 	cfs_rq->removed.util_avg += se->avg.util_avg;
 	cfs_rq->removed.runnable_avg += se->avg.runnable_avg;
-	raw_spin_unlock(&cfs_rq->removed.lock);
+	raw_spin_unlock_irqrestore(&cfs_rq->removed.lock, flags);
 }
 
 static inline bool load_avg_is_decayed(struct sched_avg *sa)
