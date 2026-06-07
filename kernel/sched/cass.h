7ad1e8003bc61f15cf1075ba12cac30f96649865
deu <fawwazzuladhim700@gmail.com>
2026-06-07 22:45:58 +0700

    sched/cass: cleanup header stub
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/cass.h
+++ kernel/sched/cass.h
@@ -5,14 +5,6 @@
 */
 #ifndef _CASS_4_14_COMPAT
 #define _CASS_4_14_COMPAT
-
 #include <linux/sched.h>
 #include <linux/cpuidle.h>
-
-#ifndef arch_scale_min_freq_capacity
-static inline unsigned long arch_scale_min_freq_capacity(int cpu)
-{
-    return arch_scale_cpu_capacity(cpu);
-}
-#endif
 #endif /* _CASS_4_14_COMPAT */
