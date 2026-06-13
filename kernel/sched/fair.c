b47561d8d5c82c1c3130bfbe6324a78d669c99e8
deu <fawwazzuladhim700@gmail.com>
2026-06-13 22:02:49 +0700

    Revert "sched: increase migration_cost to 500000"
    This reverts commit d20dad9da734bb56c42b986d64690989528674ab.
    
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -115,7 +115,7 @@ unsigned int __read_mostly sysctl_sched_energy_aware = 1;
  */
 unsigned int sysctl_sched_child_runs_first __read_mostly;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 0UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_SCHED_WALT
