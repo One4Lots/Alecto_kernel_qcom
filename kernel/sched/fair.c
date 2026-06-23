9be808a1b25a80366127aa6d5493c5b93bc287b5
deu <fawwazzuladhim700@gmail.com>
2026-06-23 20:50:44 +0700

    sched/fair: fix arch_scale_max_freq
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10583,7 +10583,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = arch_scale_cpu_capacity(cpu);
 	struct sched_group *sdg = sd->groups;
 
-	capacity *= arch_scale_max_freq_capacity(cpu);
+	capacity *= arch_scale_max_freq_capacity(sd, cpu);
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
 	capacity = min(capacity, thermal_cap(cpu));
