2a9e4020488c156e5976799e285573d6116fac6d
deu <fawwazzuladhim700@gmail.com>
2026-06-25 05:09:43 +0700

    FIXUP: restore missing in_iowait decrement
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2597,7 +2597,12 @@ ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags,
 #ifdef CONFIG_SMP
 	if (wake_flags & WF_MIGRATED)
 		en_flags |= ENQUEUE_MIGRATED;
+	else
 #endif
+	if (p->in_iowait) {
+		delayacct_blkio_end(p);
+		atomic_dec(&task_rq(p)->nr_iowait);
+	}
 
 	ttwu_activate(rq, p, en_flags);
 	ttwu_do_wakeup(rq, p, wake_flags, rf);
