6e51e5009683e672b0f984016a64297a0aca35d1
deu <fawwazzuladhim700@gmail.com>
2026-07-02 20:40:01 +0700

    FIXUP: sched: rt&dl: explicitly update clock when re-acquiring the lock
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1732,6 +1732,9 @@ pick_next_task_dl(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		 * means a stop task can slip in, in which case we need to
 		 * re-start task selection.
 		 */
+		rq->clock_update_flags &= ~(RQCF_ACT_SKIP|RQCF_REQ_SKIP);
+		update_rq_clock(rq);
+
 		if (rq->stop && task_on_rq_queued(rq->stop))
 			return RETRY_TASK;
 	}
--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1747,6 +1747,9 @@ pick_next_task_rt(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		 * means a dl or stop task can slip in, in which case we need
 		 * to re-start task selection.
 		 */
+		rq->clock_update_flags &= ~(RQCF_ACT_SKIP|RQCF_REQ_SKIP);
+		update_rq_clock(rq);
+
 		if (unlikely((rq->stop && task_on_rq_queued(rq->stop)) ||
 			     rq->dl.dl_nr_running))
 			return RETRY_TASK;
