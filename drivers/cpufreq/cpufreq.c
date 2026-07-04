c6c731f18c6aa38c41e6224969ac0e84d06afd56
deu <fawwazzuladhim700@gmail.com>
2026-07-04 18:34:45 +0700

    FAS: register FAS kconfig.
    when CONFIG_SCHED_FAS is enabled, it will compiles fas.o and disables userspace flooring entirely
    
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -675,6 +675,7 @@ show_one(cpuinfo_min_freq, cpuinfo.min_freq);
 show_one(cpuinfo_max_freq, cpuinfo.max_freq);
 show_one(cpuinfo_transition_latency, cpuinfo.transition_latency);
 
+#ifdef CONFIG_SCHED_FAS
 /*
  * A hack that best with FAS and disabled userspace flooring.
  */
@@ -682,6 +683,9 @@ static ssize_t show_scaling_min_freq(struct cpufreq_policy *policy, char *buf)
 {
 	return sprintf(buf, "%u\n", policy->user_policy.min);
 }
+#else
+show_one(scaling_min_freq, min);
+#endif
 show_one(scaling_max_freq, max);
 
 __weak unsigned int arch_freq_get_on_cpu(int cpu)
