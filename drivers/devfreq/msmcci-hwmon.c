1d50d14d79ad9d66636661b8febdd3fd83658f01
deu <fawwazzuladhim700@gmail.com>
2026-07-15 21:45:02 +0700

    devfreq: msmcci-hwmon: add update_lock to shared IRQ handler
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- drivers/devfreq/msmcci-hwmon.c
+++ drivers/devfreq/msmcci-hwmon.c
@@ -171,7 +171,10 @@ static irqreturn_t msmcci_hwmon_shared_intr_handler(int irq, void *dev)
 	if (idx == -1)
 		return IRQ_NONE;
 
-	update_cache_hwmon(&m->hw);
+	mutex_lock(&m->update_lock);
+	if (mon_is_match_flag_set(m, idx))
+		update_cache_hwmon(&m->hw);
+	mutex_unlock(&m->update_lock);
 
 	return IRQ_HANDLED;
 }
@@ -394,7 +397,7 @@ static int register_pm_notifier(struct msmcci_hwmon *m)
 	return 0;
 }
 
-static void unregister_pm_nofitifier(void)
+static void unregister_pm_notifier(void)
 {
 	mutex_lock(&notifier_reg_lock);
 	use_cnt--;
@@ -459,7 +462,7 @@ static int start_hwmon(struct cache_hwmon *hw, struct mrps_stats *mrps)
 		ret = request_interrupts(m);
 
 	if (ret) {
-		unregister_pm_nofitifier();
+		unregister_pm_notifier();
 		return ret;
 	}
 	mon_init(m);
@@ -491,7 +494,7 @@ static void stop_hwmon(struct cache_hwmon *hw)
 		mon_clear_single(m, i);
 	}
 
-	unregister_pm_nofitifier();
+	unregister_pm_notifier();
 }
 
 static int msmcci_hwmon_parse_dt(struct platform_device *pdev,
