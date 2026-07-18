adb7d030304c1a552504fa4d408f3efa21a9ff6b
deu <fawwazzuladhim700@gmail.com>
2026-07-18 18:29:20 +0700

    sched: cleanups
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -81,7 +81,6 @@
 
 #include "cpupri.h"
 #include "cpudeadline.h"
-#include "cpuacct.h"
 #include "features.h"
 
 #ifdef CONFIG_SCHED_DEBUG
@@ -3817,3 +3816,21 @@ struct sched_avg_stats {
 	int nr_max;
 };
 extern void sched_get_nr_running_avg(struct sched_avg_stats *stats);
+
+#ifdef CONFIG_CGROUP_CPUACCT
+
+extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
+extern void cpuacct_account_field(struct task_struct *tsk, int index, u64 val);
+
+#else
+
+static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime)
+{
+}
+
+static inline void
+cpuacct_account_field(struct task_struct *tsk, int index, u64 val)
+{
+}
+
+#endif
