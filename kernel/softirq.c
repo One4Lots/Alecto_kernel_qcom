90a62b764a03606c8953da40219d0413cd141554
deu <fawwazzuladhim700@gmail.com>
2026-07-30 05:07:26 +0700

    TEMP: catch who leave irq enabled state
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -403,11 +403,8 @@ static inline void tick_irq_exit(void)
  */
 void irq_exit(void)
 {
-#ifndef __ARCH_IRQ_EXIT_IRQS_DISABLED
-	local_irq_disable();
-#else
-	lockdep_assert_irqs_disabled();
-#endif
+	if (WARN_ON_ONCE(!irqs_disabled()))
+		local_irq_disable();
 	account_irq_exit_time(current);
 	preempt_count_sub(HARDIRQ_OFFSET);
 	if (!in_interrupt() && local_softirq_pending())
