SuperiorOS-Devices/kernel_xiaomi_sweet

power: supply: maxim: Fix invalid casting from a pointer to integer.
../drivers/power/supply/maxim/onewire_gpio.c:465:84: warning: cast to smaller integer type 'uint32_t' (aka 'unsigned int') from 'void *' [-Wvoid-pointer-to-int-cast]
        ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg66_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
                                                                                          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
../drivers/power/supply/maxim/onewire_gpio.c:28:16: note: expanded from macro 'ow_log'
                ^
../include/linux/printk.h:306:33: note: expanded from macro 'pr_err'
        printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__)
                                       ^~~~~~~~~~~
../drivers/power/supply/maxim/onewire_gpio.c:465:126: warning: cast to smaller integer type 'uint32_t' (aka 'unsigned int') from 'void *' [-Wvoid-pointer-to-int-cast]
        ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg66_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
                                                                                                                                    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
../drivers/power/supply/maxim/onewire_gpio.c:28:16: note: expanded from macro 'ow_log'
                ^
../include/linux/printk.h:306:33: note: expanded from macro 'pr_err'
        printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__)
                                       ^~~~~~~~~~~
  CC      drivers/media/rc/keymaps/rc-tbs-nec.o
  CC      drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_top/cam_vfe_top_ver2.o
2 warnings generated.

Signed-off-by: Shashank Patil <9945shashank@gmail.com>

drivers/power/supply/maxim/onewire_gpio.c
@@ -458,11 +458,11 @@ static int onewire_gpio_probe(struct platform_device *pdev)
 	onewire_data->ow_gpio_chip = gpiod_to_chip(onewire_data->ow_gpio_desc);
 
 	onewire_data->gpio_in_out_reg = devm_ioremap(&pdev->dev,
-					(uint32_t)onewire_data->onewire_gpio_level_addr, 0x4);
+					(uintptr_t)onewire_data->onewire_gpio_level_addr, 0x4);
 	onewire_data->gpio_cfg66_reg = devm_ioremap(&pdev->dev,
-					(uint32_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
-	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uint32_t)(onewire_data->onewire_gpio_level_addr), (uint32_t)(onewire_data->onewire_gpio_cfg_addr));
-	ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg66_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
+					(uintptr_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
+	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uintptr_t)(onewire_data->onewire_gpio_level_addr), (uintptr_t)(onewire_data->onewire_gpio_cfg_addr));
+	ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uintptr_t)(onewire_data->gpio_cfg66_reg), (uintptr_t)(onewire_data->gpio_in_out_reg));
 
 	// create device node
 	onewire_data->dev = device_create(onewire_class,

One4Lots/Alecto_kernel_qcom

Signed-off-by: One4Lots <sahorkot.ontime@gmail.com>

drivers/power/supply/maxim/onewire_gpio.c
@@ -442,11 +442,11 @@ static int onewire_gpio_probe(struct platform_device *pdev)
 	onewire_data->ow_gpio_chip = gpiod_to_chip(onewire_data->ow_gpio_desc);
 	
 	onewire_data->gpio_in_out_reg = devm_ioremap(&pdev->dev,
-					(uint32_t)onewire_data->onewire_gpio_level_addr, 0x4);
+					(uintptr_t)onewire_data->onewire_gpio_level_addr, 0x4);
 	onewire_data->gpio_cfg_reg = devm_ioremap(&pdev->dev,
-					(uint32_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
-	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uint32_t)(onewire_data->onewire_gpio_level_addr), (uint32_t)(onewire_data->onewire_gpio_cfg_addr));
-	ow_log("onewire_data->gpio_cfg_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
+					(uintptr_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
+	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uintptr_t)(onewire_data->onewire_gpio_level_addr), (uintptr_t)(onewire_data->onewire_gpio_cfg_addr));
+	ow_log("onewire_data->gpio_cfg_reg is %x; onewire_data->gpio_in_out_reg is %x", (uintptr_t)(onewire_data->gpio_cfg_reg), (uintptr_t)(onewire_data->gpio_in_out_reg));
 	
 	// create device node
 	onewire_data->dev = device_create(onewire_class,