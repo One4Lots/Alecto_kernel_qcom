10838461becbdff289551168ca0ac0e66f53dffe
One4Lots <sahorkot.ontime@gmail.com>
2026-08-23 13:16:20 +0600

    Revert "FAS: boost all cores on >30hz"
    This reverts commit 8504eb9f7f01373d2cca51af3e397dd0bec55b80.

--- kernel/sched/fas.c
+++ kernel/sched/fas.c
@@ -114,8 +114,7 @@ static void fas_do_boost(struct work_struct *work)
 		if (fps <= 30) {
 			s->boost_min = (i <= 5) ? 768000 : 0;
 		} else {
-			s->boost_min = (i <= 5) ? 1324800 :
-				       (i <= 7) ? 825600 : 0;
+			s->boost_min = (i <= 5) ? 1324800 : 0;
 		}
 	}
 
